e9b6f01e2ff8c1fa360c4c7c5c748a6d3874a030
Carlo Dee <carlodee.official@gmail.com>
2022-10-18 15:54:09 +0800

    cannong-twrp: initial commit - adapted to miui v13.0.2
    Signed-off-by: Carlo Dandan <jasminecarlo01@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_cannong.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_cannong-user \
+    twrp_cannong-userdebug \
+    twrp_cannong-eng
---
+++ twrp_cannong.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from cannong device
+$(call inherit-product, device/xiaomi/cannong/device.mk)
+
+PRODUCT_DEVICE := cannong
+PRODUCT_NAME := twrp_cannong
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 9T
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="cannong-user 12 SP1A.210812.016 V13.0.2.0.SJEMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/cannong_global/cannong:12/SP1A.210812.016/V13.0.2.0.SJEMIXM:user/release-keys
