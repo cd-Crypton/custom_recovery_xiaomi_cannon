f68336bdb071fbaf1dded137a71cad17c9eb7a07
Carlo Dee <carlodee.official@gmail.com>
2022-10-18 18:25:26 +0800

    Import additional libs and modules.

--- device.mk
+++ device.mk
@@ -6,3 +6,26 @@
 #
 
 LOCAL_PATH := device/xiaomi/cannong
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster41 \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Keystore
+PRODUCT_PACKAGES += \
+    android.system.keystore2
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
