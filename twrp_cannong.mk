ba4e5cba0083a54329185cb37aa51777f84566c5
Carlo Dee <carlodee.official@gmail.com>
2022-10-20 01:01:46 +0800

    Added some inherit-if-exists.

--- twrp_cannong.mk
+++ twrp_cannong.mk
@@ -6,12 +6,15 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common twrp/pb/omni stuff.
 $(call inherit-product-if-exists, vendor/twrp/config/common.mk)
-$(call inherit-product-if-exists, vendor/pbrp/config/common.mk)
+$(call inherit-product-if-exists, vendor/pb/config/common.mk)
 $(call inherit-product-if-exists, vendor/omni/config/common.mk)
 
 # Inherit from cannong device
