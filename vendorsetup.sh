063f3b941f9787cbb303bb262a98864a18df9006
Carlo Dandan <115797725+cd-Spidey@users.noreply.github.com>
2022-10-21 22:35:43 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#add_lunch_combo twrp_cannong-eng
-
 FDEVICE="cannong"
 #set -o xtrace
 
