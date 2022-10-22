3c659b0c79953e10d78598c55d7e3e65baabe201
Carlo Dandan <115797725+cd-Spidey@users.noreply.github.com>
2022-10-23 00:10:04 +0800

    Update twrp_cannong.mk

--- twrp_cannong.mk
+++ twrp_cannong.mk
@@ -7,11 +7,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common twrp/pb/omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from cannong device
 $(call inherit-product, device/xiaomi/cannong/device.mk)
