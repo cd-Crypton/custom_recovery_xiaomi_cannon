9d930262666829cc9c7ac4df211310bbe726d76f
Carlo Dandan | Not a Developer <115797725+cd-Spidey@users.noreply.github.com>
2022-12-07 23:11:50 +0800

    Major clean up.
    Reorganized the looks, and removed product inherit that are not really needed in building twrp.

--- twrp_cannong.mk
+++ twrp_cannong.mk
@@ -1,35 +1,36 @@
-#
 # Copyright (C) 2022 The Android Open Source Project
 # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
-#
 
-# Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Inherit some common twrp/pb/omni stuff.
-$(call inherit-product-if-exists, vendor/twrp/config/common.mk)
-$(call inherit-product-if-exists, vendor/pb/config/common.mk)
-$(call inherit-product-if-exists, vendor/omni/config/common.mk)
-
-# Inherit from cannong device
-$(call inherit-product, device/xiaomi/cannong/device.mk)
-
-PRODUCT_RELEASE_NAME := cannong
 PRODUCT_DEVICE := cannong
-PRODUCT_NAME := twrp_cannong
-PRODUCT_BRAND := Redmi
+PRODUCT_NAME := twrp_$(PRODUCT_DEVICE)
+PRODUCT_BRAND := redmi
 PRODUCT_MODEL := Redmi Note 9T
 PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_RELEASE_NAME := $(PRODUCT_DEVICE)
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+DEVICE_PATH := device/$(PRODUCT_MANUFCATURER)/$(PRODUCT_DEVICE)
+
+# Inherit from the Device Tree itself.
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Inherit from Prebuilt Products.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit from TWRP-common Stuff, if building TWRP.
+$(call inherit-product-if-exists, vendor/twrp/config/common.mk)
+
+# Inherit from PBRP-common Stuff, if building PBRP.
+$(call inherit-product-if-exists, vendor/pb/config/common.mk)
+
+# Inherit from OMNI-common Stuff, if building from below Android 10.
+$(call inherit-product-if-exists, vendor/omni/config/common.mk)
+
+PRODUCT_GMS_CLIENTID_BASE := android-$(PRODUCT_MANUFCATURER)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cannong-user 12 SP1A.210812.016 V13.0.2.0.SJEMIXM release-keys"
+    PRIVATE_BUILD_DESC="$(PRODUCT_DEVICE)-user 12 SP1A.210812.016 V13.0.2.0.SJEMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/cannong_global/cannong:12/SP1A.210812.016/V13.0.2.0.SJEMIXM:user/release-keys
+BUILD_FINGERPRINT := $(PRODUCT_BRAND)/$(PRODUCT_DEVICE)/$(PRODUCT_DEVICE):12/SP1A.210812.016/V13.0.2.0.SJEMIXM:user/release-keys
