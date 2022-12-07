fa6e4b347329429d412ca33faf2d120d901a7240
Carlo Dandan | Not a Developer <115797725+cd-Spidey@users.noreply.github.com>
2022-12-07 23:19:21 +0800

    Moved ota asserts in device.mk.

--- device.mk
+++ device.mk
@@ -1,9 +1,6 @@
-#
 # Copyright (C) 2022 The Android Open Source Project
 # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
-#
 
 LOCAL_PATH := device/xiaomi/cannong
 
@@ -14,3 +11,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := cannon,cannong,cannong_global
