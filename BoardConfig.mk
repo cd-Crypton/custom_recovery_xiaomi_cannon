d3d801c4cd8d60d058893e3d4e7668d1b6034c8c
Carlo Dandan <jasminecarlo01@gmail.com>
2023-01-31 19:47:33 +0800

    Update BoardConfig.mk
    Signed-off-by: Carlo Dandan <jasminecarlo01@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,6 @@
 # Copyright (C) 2022 The Android Open Source Project
 # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: Apache-2.
 
 DEVICE_PATH := device/xiaomi/cannong
 
